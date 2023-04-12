c748217147a38bf5dd8dd622bd5fa29bcafc5d92
dothedro <drooooofficial@gmail.com>
2023-04-12 09:57:24 +0800

    fixed: image path

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
     export default {
         data() {
             return {
-                siteLogo: '.././src/assets/images/logo-image.png'
+                siteLogo: './src/assets/images/logo-image.png'
             }
         }
     }
